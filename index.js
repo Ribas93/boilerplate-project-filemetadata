dafeb4e54f45de35f1f2c7e1b15bdb386fd7843e
Ribas93 <ribasoficial01@gmail.com>
2022-08-19 10:43:16 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-var express = require('express');
-var cors = require('cors');
+let express = require('express');
+let cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(express.urlencoded({extended:true}))
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({dest: 'arquivos'})
 
+
+app.post("/api/fileanalyse",upload.single('upfile'),(req,res) =>{
+
+  const {originalname,mimetype,size } = req.file
+
+  res.json({name:originalname,type:mimetype,size:size})
+})
 
 
 const port = process.env.PORT || 3000;
